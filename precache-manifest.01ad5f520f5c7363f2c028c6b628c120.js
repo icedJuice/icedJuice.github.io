5ac06c2939270ebd9aea510e1c426c4b3cb638bc
hanguozhi <hanguozhi@bestcode.com>
2018-12-27 15:11:03 +0800

    mf

---
+++ precache-manifest.01ad5f520f5c7363f2c028c6b628c120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8eb73e03afc5e6831c6",
+    "url": "/static/js/main.b8eb73e0.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b8eb73e03afc5e6831c6",
+    "url": "/static/css/main.3333f6bb.chunk.css"
+  },
+  {
+    "revision": "1255572b4887a5ebe505f916ec02e9c1",
+    "url": "/index.html"
+  }
+];
